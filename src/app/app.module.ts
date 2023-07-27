801f111d551e20d4121542470fbe2f224ce4cd14
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-07-26 23:53:54 -0300

    add component header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
   HomeComponent,
+  HeaderComponent,
 	],
 	imports: [
 		BrowserModule,
